7d150fb43687b87ea553dd4b8ec6f6d51c796515
Raphael Rouvinov-Kats <coolcade283@gmail.com>
2015-07-12 02:28:58 -0500

    added document.ready

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
-$("body").mousemove(function(e){
-  var pageCoords = "( " + e.pageX + ", " + e.pageY + " )";
-  //var clientCoords = "( " + e.clientX + ", " + e.clientY + " )";
-  $("img").attr('width', e.pageX);
-  $("img").attr('height', e.pageY);
+$(document).ready(function() {
+  $("body").mousemove(function(e){
+    var pageCoords = "( " + e.pageX + ", " + e.pageY + " )";
+    //var clientCoords = "( " + e.clientX + ", " + e.clientY + " )";
+    $("img").attr('width', e.pageX);
+    $("img").attr('height', e.pageY);
+  });
 });
