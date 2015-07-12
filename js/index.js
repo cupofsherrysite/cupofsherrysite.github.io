4bf05857aed0a7f1fc0c08c4d283cbfe7b9b6701
Raphael Rouvinov-Kats <coolcade283@gmail.com>
2015-07-12 03:17:58 -0500

    touch-friendly js

--- js/index.js
+++ js/index.js
@@ -6,5 +6,14 @@ $(document).ready(function() {
     $("img").attr('height', e.pageY);
   });
   
-  document.ontouchmove = function(e) {e.preventDefault()};
+  //document.ontouchmove = function(e) {e.preventDefault()};
+  
+  document.getElementsByTagName('body')[0].addEventListener('touchmove', function(event) {
+    var touch = event.targetTouches[0];
+ 
+    // Place element where the finger is
+    $("img").attr('width', touch.pageX);
+     $("img").attr('height', touch.pageX);
+    event.preventDefault();
+  }, false);
 });
