4358fc68d2b81961c765c134448fb9d8a7315ccc
Raphael Rouvinov-Kats <coolcade283@gmail.com>
2015-07-12 02:26:40 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,6 @@
-
+$("body").mousemove(function(e){
+  var pageCoords = "( " + e.pageX + ", " + e.pageY + " )";
+  //var clientCoords = "( " + e.clientX + ", " + e.clientY + " )";
+  $("img").attr('width', e.pageX);
+  $("img").attr('height', e.pageY);
+});
